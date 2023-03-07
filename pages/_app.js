59a637cdd35bae5dc84a627d228a6ceaffc1237b
Fahim Iftekhar Efto <ifto.141.fi@gmail.com>
2023-03-08 02:13:14 +0600

    spinner fixed

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,25 @@ import NextNProgress from "nextjs-progressbar";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <NextNProgress color="#a5002f" height={5} />
+      <NextNProgress
+        color="#a5002f"
+        height={5}
+        transformCSS={(css) => {
+          const modifiedCSS = `
+            ${css}
+            #nprogress .spinner {
+              display: block;
+              position: fixed;
+              z-index: 1031;
+              top: 25%;
+              size: 50px;
+              right: 50%;
+            }
+            
+          `;
+          return <style>{modifiedCSS}</style>;
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
