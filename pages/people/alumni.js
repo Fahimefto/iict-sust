6d383aa3c44b4720162078deed396d228a455afe
Farhan Mahtab Mahi <farhan.mahi1999@gmail.com>
2023-03-12 22:45:12 +0600

    Alumni api integration done

--- pages/people/alumni.js
+++ pages/people/alumni.js
@@ -1,48 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/common/Layout";
 import HeadingBar from "../../components/common/headingBar";
 import Link from "next/link";
-
+import Axios from "axios";
 export default function Alumni() {
-  const alumnis = [
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-    {
-      bacth: "Praromvik",
-      session: "2016-17",
-    },
-  ];
+  const [alumni, setAlumni] = useState([]);
+
+  async function getAlumni() {
+    try {
+      const response = await Axios.get("http://localhost:5000/api/alumni");
+      console.log(response);
+      const data = response.data;
+      setAlumni(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    getAlumni();
+  }, []);
+  console.log(getAlumni);
   return (
     <Layout>
       <HeadingBar name="Alumni" />
       <div className="mt-10">
-        {alumnis.map((alumni, index) => (
-          <Link href="#" key={index}>
+        {alumni.map((alumni) => (
+          <Link href="#" key={alumni.id}>
             <div className="max-w-screen-xl lg:mx-auto border-2 border-rose-800 p-2 rounded-md text-center xs:mx-10 mb-5">
               <h2 className="font-bold text-xl text-gray-800">
-                {alumni.bacth}
+                {alumni.batchName}
               </h2>
               <h2 className="text-gray-600 ">Session: {alumni.session}</h2>
               <button className="bg-rose-900 rounded-full px-3 py-2  font-bold mt-2 text-white hover:scale-105">
-                See More
+                See More{alumni.file}
               </button>
             </div>
           </Link>
